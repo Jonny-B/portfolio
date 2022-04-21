a588280094e8d8efad119311fb579b040521a61b
Jonny B <jonthan.blewitt@covermymeds.com>
2022-04-21 09:53:14 -0400

    initial commit

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname,'./builds')))
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendFile(path.join(__dirname, '..', 'builds', 'build', 'index.html'))
 });
 
 module.exports = router;
