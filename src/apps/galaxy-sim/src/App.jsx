8c2b8ff420286b4a54cf4cefaf9ffc45d6dd524c
Jonny B <jonthan.blewitt@covermymeds.com>
2022-05-05 15:24:36 -0700

    Adding react bootstrap to galaxy sim

--- src/apps/galaxy-sim/src/App.jsx
+++ src/apps/galaxy-sim/src/App.jsx
@@ -1,5 +1,6 @@
 import {Navbar, Container} from 'react-bootstrap'
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
           <Navbar.Brand href="#home">
             <img
               alt=""
-              src="/logo.svg"
+              src={logo}
               width="30"
               height="30"
               className="d-inline-block align-top"
