891b91ae909b653a2ebb21bf5e797c5cb03bda25
jonny-b <jonlblewitt@gmail.com>
2023-04-16 22:04:00 -0400

    Adding custom Vector class. This allows me to remove using p5. This helps me better understand what is going on.

---
+++ src/apps/galaxy-sim/src/simulation/Vector.ts
@@ -0,0 +1,57 @@
+export default class Vector {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+  }
+
+  static sub(v1: Vector, v2: Vector): Vector {
+    return new Vector(v1.x - v2.x, v1.y - v2.y);
+  }
+
+  static div(v: Vector, n: number): Vector {
+    return new Vector(v.x / n, v.y / n);
+  }
+
+  static constrain(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+  }
+
+
+
+  setMag(len: number): Vector {
+    const m = this.mag();
+    if (m !== 0) {
+      this.mult(len / m);
+    }
+    return this;
+  }
+
+  mag(): number {
+    return Math.sqrt(this.magSq());
+  }
+
+  magSq(): number {
+    return this.x * this.x + this.y * this.y;
+  }
+
+  add(value: Vector | number[]): Vector {
+    if (value instanceof Vector) {
+      this.x += value.x;
+      this.y += value.y;
+    } else {
+      this.x += value[0];
+      this.y += value[1];
+    }
+    return this;
+  }
+
+
+  mult(n: number): Vector {
+    this.x *= n;
+    this.y *= n;
+    return this;
+  }
+}
